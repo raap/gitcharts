dfe70be96515122d7e61f60b79f5315862977208
Petr Volny <volny.petr@gmail.com>
2014-12-02 22:26:38 +0100

    Dashboard coding/styling

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+    init: function() {
+        this._super();
+        this.set('days', 21);
+    },
+
     contentChanged: function() {
         // TODO: Model transformations here
     }.observes('content')
